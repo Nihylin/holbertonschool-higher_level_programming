1dbc3e493d98bdada9abf8f6b3735dfb0544b0b5
Nihylin <nihylin@gmail.com>
2024-01-26 12:42:03 +0100

    Add tasks 1 through 12 Python programs + editorconfig file

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastD = abs(number) % 10
+if number < 0:
+    lastD = -lastD
+if lastD < 6:
+    print(f"Last digit of {number} is {lastD} and is less than 6 and not 0")
+elif lastD == 0:
+    print(f"Last digit of {number} is {lastD} and is 0")
+elif lastD > 5:
+    print(f"Last digit of {number} is {lastD} and is greater than 5")
