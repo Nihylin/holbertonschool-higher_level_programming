6253edcaea34f5aa2e9e64060f3e380f1f7d219b
Nihylin <nihylin@gmail.com>
2024-01-26 18:02:40 +0100

    end of the week, everybody's tired, mistakes happen

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for number in range(1, 101):
-        if number % 15 == 0:
-            print("FizzBuzz", end=" ")
-        elif number % 3 == 0:
-            print("Fizz", end=" ")
-        elif number % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(number, end=" ")
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = -last_digit
+
+if last_digit == 0:
+    end_string = 'is 0'
+elif last_digit > 5:
+    end_string = 'is greater than 5'
+else:
+    end_string = 'is less than 6 and not 0'
+
+print(f"Last digit of {number} is {last_digit} and {end_string}")
