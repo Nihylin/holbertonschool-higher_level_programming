4207b4edcb6c7be1f0df6658de0778e0a55a579d
Nihylin <nihylin@gmail.com>
2024-01-30 14:42:04 +0100

    Add "Lists of lists = Matrix" Python program

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        i = 0
+        for num in row:
+            print("{:d}".format(num), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+            i += 1
+        print()
