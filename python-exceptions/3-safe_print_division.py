6d25b684d0bccedcd3a02a7f428e16ed9360aec4
Nihylin <nihylin@gmail.com>
2024-02-02 17:36:56 +0100

    Add "Integers division with debug" Python program

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        divisionResult = a / b
+    except (ValueError, TypeError, ZeroDivisionError):
+        divisionResult = None
+    finally:
+        print("Inside result: {}".format(divisionResult))
+
+    return divisionResult
