a632813230e78f594a8578aa67ca6c4ce8d2e1b8
Nihylin <nihylin@gmail.com>
2024-02-06 15:43:37 +0100

    Add "Area of a square" Python program

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """I mean this is pretty self explanatory"""
+
+    def __init__(self, size=0):
+        """Square class constructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
