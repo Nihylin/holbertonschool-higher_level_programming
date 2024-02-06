05b063ea049197444b29d58f7aebc924bcedf516
Nihylin <nihylin@gmail.com>
2024-02-06 14:39:18 +0100

    Add "Square with size" Python program

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """I mean this is pretty self explanatory"""
+
+    def __init__(self, size):
+        """Square class constructor"""
+        self.__size = size
