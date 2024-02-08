c04e5d0c4fc0aba2e6ca975ebefc0a57c9e72234
Nihylin <nihylin@gmail.com>
2024-02-08 01:15:28 +0100

    Add "Integers addition" Python program + test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Defines an integer addition function."""
+
+
+def add_integer(a, b=98):
+    """Addition of two variables"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return int(a + b)
