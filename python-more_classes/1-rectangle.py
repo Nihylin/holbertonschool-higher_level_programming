e84797bed10e8621a71f4ca4a72c90632437124d
Nihylin <nihylin@gmail.com>
2024-02-11 21:44:37 +0100

    Add "Real definition of a rectangle" Python program

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""
+defines a rectangle
+"""
+
+
+class Rectangle:
+    """defines a rectangle by width and height"""
+
+    def __init__(self, width=0, height=0):
+        """Rectangle class constructor"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width of rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
