123a6f01dfe85618b44114673b86899cd413c20a
Nihylin <nihylin@gmail.com>
2024-02-11 22:43:25 +0100

    refactor "Print square" Python program

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,11 +13,8 @@ def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
-        raise TypeError("size must be >= 0")
+        raise ValueError("size must be >= 0")
 
-    for _ in range(size):
-        for _ in range(size):
-            print("#", end="")
-        print("")
+    for i in range(size):
+        print("#" * size)
